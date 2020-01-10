7c77d1d6a559d0d4ab539c7872d3afcdec83e885
Jorge Aparicio <jorge.aparicio@ferrous-systems.com>
2020-01-10 20:19:15 +0100

    fix doc comment

--- firmware/cortex-a/src/register/scr.rs
+++ firmware/cortex-a/src/register/scr.rs
@@ -1,10 +1,8 @@
 //! Security Control Register
 
-//! Reads the SCR register
+/// Reads the SCR register
 pub fn read() -> u16 {
     let scr;
-    unsafe {
-        asm!("mrc p15, 0, $0, c1, c1, 0" : "=r"(scr));
-    }
+    unsafe { asm!("mrc p15, 0, $0, c1, c1, 0" : "=r"(scr)) }
     scr
 }
