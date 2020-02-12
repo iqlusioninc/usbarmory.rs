6f186708d843cd7e031ea29944f64f6c9e998a53
Jorge Aparicio <jorge.aparicio@ferrous-systems.com>
2020-02-12 20:44:17 +0100

    USB enumeration
    also fix a bug in Instant.duration_since
    and make panic-serial flush the in-memory logger

---
+++ firmware/usbarmory/src/usbd/dqh.rs
@@ -0,0 +1,244 @@
+use core::{
+    cell::{Cell, UnsafeCell},
+    fmt, ptr,
+    sync::atomic::{self, Ordering},
+};
+
+use super::{
+    dtd::{dTD, NO_NEXT_DTD},
+    token::Token,
+    util::{Ref, Reserved},
+};
+
+const SETUP_BYTES: usize = 8;
+
+/// Endpoint Queue Header
+// Memory layout is specified in table 54-56 of ULRM
+// NOTE(align) "The dQH is a 48-byte data structure, but must be aligned on
+// 64-byte boundaries"
+// NOTE all instances of this struct are shared with the hardware; instances of
+// this type should NEVER appear behind an exclusive reference (`&mut-`)
+#[allow(non_camel_case_types)]
+#[repr(align(64))]
+#[repr(C)]
+pub struct dQH {
+    caps: Cell<Caps>,
+    // NOTE these fields can be modified by the hardware. Care needs to be
+    // exercised when accessing these fields. In particular, memory barriers are
+    // required to avoid data races with the hardware and cache invalidation is
+    // required to read the actual state of these fields.
+    current_dtd: UnsafeCell<usize>,
+    next_dtd: UnsafeCell<usize>,
+    token: UnsafeCell<Token>,
+    pages: [UnsafeCell<usize>; 5],
+    // must always be zero
+    reserved: UnsafeCell<Reserved>,
+    // SETUP packets (see USB control transfers) are written to this filed
+    setup: [UnsafeCell<u8>; SETUP_BYTES],
+    // XXX I think we are allowed to use the 16 bytes that follow and are used
+    // for padding (the hardware requires that dQHs are laid down in an array)
+}
+
+impl dQH {
+    pub const SETUP_BYTES: usize = SETUP_BYTES;
+
+    pub const fn new() -> Self {
+        dQH {
+            caps: Cell::new(Caps::empty()),
+            current_dtd: UnsafeCell::new(1),
+            next_dtd: UnsafeCell::new(1),
+            token: UnsafeCell::new(Token::empty()),
+            pages: [
+                UnsafeCell::new(0),
+                UnsafeCell::new(0),
+                UnsafeCell::new(0),
+                UnsafeCell::new(0),
+                UnsafeCell::new(0),
+            ],
+            reserved: UnsafeCell::new(Reserved::new()),
+            setup: [
+                UnsafeCell::new(0),
+                UnsafeCell::new(0),
+                UnsafeCell::new(0),
+                UnsafeCell::new(0),
+                UnsafeCell::new(0),
+                UnsafeCell::new(0),
+                UnsafeCell::new(0),
+                UnsafeCell::new(0),
+            ],
+        }
+    }
+
+    /// Clears all the fields
+    ///
+    /// # Safety
+    ///
+    /// Must be called only when the hardware is not operating on the dQH. Must
+    /// be synchronized with a memory barrier before letting the hardware access
+    /// this field
+    pub unsafe fn clear(&self) {
+        self.caps.set(Caps::empty());
+        self.current_dtd.get().write(1);
+    }
+
+    /// # Safety
+    ///
+    /// Must be called only when the hardware is not operating on the dQH. Must
+    /// be synchronized with a memory barrier before letting the hardware read
+    /// this field
+    pub unsafe fn set_max_packet_size(&self, max_packet_size: u16) {
+        self.caps.set(Caps::new(max_packet_size));
+    }
+
+    /// # Safety
+    ///
+    /// The returned reference must be destroyed before the hardware is able to
+    /// modify the dQH value
+    pub unsafe fn get_current_dtd(&self) -> Option<Ref<dTD>> {
+        let p = self.current_dtd.get().read();
+        if p & 0b11111 != 0 {
+            // garbage is currently stored in this field -- i.e. the hardware
+            // has not yet written a value to it
+            None
+        } else {
+            Some(Ref::new_unchecked(p as *const dTD))
+        }
+    }
+
+    /// # Safety
+    ///
+    /// The returned reference must be destroyed before the hardware is able to
+    /// modify the dQH value
+    pub unsafe fn get_next_dtd(&self) -> Option<Ref<dTD>> {
+        let p = self.next_dtd.get().read();
+        if p == NO_NEXT_DTD {
+            // terminate bit is set
+            None
+        } else {
+            Some(Ref::new_unchecked(p as *const dTD))
+        }
+    }
+
+    pub unsafe fn get_token(&self) -> Token {
+        self.token.get().read()
+    }
+
+    /// # Safety
+    ///
+    /// The returned reference must be destroyed before the hardware is able to
+    /// modify the dQH value
+    pub unsafe fn clear_current_dtd(&self) {
+        self.current_dtd.get().write(NO_NEXT_DTD);
+    }
+
+    /// # Safety
+    ///
+    /// The returned reference must be destroyed before the hardware is able to
+    /// modify the dQH value
+    pub unsafe fn set_next_dtd(&self, dtd: Option<Ref<dTD>>) {
+        self.next_dtd
+            .get()
+            .write(dtd.map(|dtd| dtd.as_ptr() as usize).unwrap_or(NO_NEXT_DTD));
+    }
+
+    /// NOTE the hardware may change the setup bytes while this operation is in
+    /// progress. Check USBCMD.SUTW to see if that race occurred or not
+    pub fn copy_setup_bytes(&self, buf: &mut [u8]) {
+        // NOTE(compiler_fence) prevent this non-volatile memory operation from
+        // being reordered wrt to any other surrounding memory operation
+        atomic::compiler_fence(Ordering::SeqCst);
+        unsafe {
+            ptr::copy_nonoverlapping(self.setup[0].get(), buf.as_mut_ptr(), Self::SETUP_BYTES);
+        }
+        atomic::compiler_fence(Ordering::SeqCst);
+    }
+}
+
+// See table 54-57 of the ULRM
+#[repr(transparent)]
+#[derive(Clone, Copy)]
+pub struct Caps {
+    inner: u32,
+}
+
+impl Caps {
+    const fn empty() -> Self {
+        Self { inner: 0 }
+    }
+
+    /// Initializes the `Caps` with a configuration valid for non Isochronous
+    /// endpoints
+    pub fn new(max_packet_size: u16) -> Self {
+        assert!(max_packet_size <= 0x400);
+
+        // "Non-ISO endpoints must set Mult='00'" -- section 54.4.5.1.1
+        let mult = 0b00;
+        let zlt = 0;
+
+        Self {
+            inner: (mult << 30) | (zlt << 29) | (u32::from(max_packet_size) << 16),
+        }
+    }
+
+    pub fn max_packet_size(self) -> u16 {
+        (self.inner >> 16) as u16
+    }
+}
+
+impl fmt::Debug for Caps {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("Caps")
+            .field("max_packet_size", &self.max_packet_size())
+            .finish()
+    }
+}
+
+impl fmt::Debug for Ref<dQH> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        unsafe {
+            let current_dtd = self.current_dtd.get().read();
+            let current_dtd = if current_dtd == NO_NEXT_DTD {
+                None
+            } else {
+                Some(Ref::new_unchecked(current_dtd as *const dTD))
+            };
+
+            let next_dtd = self.next_dtd.get().read();
+            let next_dtd = if next_dtd == NO_NEXT_DTD {
+                None
+            } else {
+                Some(Ref::new_unchecked(next_dtd as *const dTD))
+            };
+
+            f.debug_struct("dQH")
+                .field("caps", &self.caps.get())
+                .field("current_dtd", &current_dtd)
+                .field("next_dtd", &next_dtd)
+                .field("token", &self.token.get().read())
+                .field(
+                    "pages",
+                    &[
+                        self.pages[0].get().read(),
+                        self.pages[1].get().read(),
+                        self.pages[2].get().read(),
+                        self.pages[3].get().read(),
+                        self.pages[4].get().read(),
+                    ],
+                )
+                .field(
+                    "setup",
+                    &[
+                        self.setup[0].get().read(),
+                        self.setup[1].get().read(),
+                        self.setup[2].get().read(),
+                        self.setup[3].get().read(),
+                        self.setup[4].get().read(),
+                        self.setup[5].get().read(),
+                        self.setup[6].get().read(),
+                        self.setup[7].get().read(),
+                    ],
+                )
+                .finish()
+        }
+    }
+}
---
+++ firmware/usbarmory/src/usbd/util.rs
@@ -0,0 +1,152 @@
+use core::{fmt, ops, ptr::NonNull, time::Duration};
+
+use usb_device::{bus::PollResult, endpoint::EndpointAddress};
+
+use crate::time::Instant;
+
+/// Aligns the inner value to a 2KB boundary
+#[repr(align(2048))]
+pub struct Align2K<T> {
+    pub inner: T,
+}
+
+/// Aligns the inner value to a 4KB boundary
+#[repr(align(4096))]
+pub struct Align4K<T> {
+    pub inner: T,
+}
+
+/// Always contains the value zero
+#[repr(transparent)]
+pub struct Reserved {
+    inner: u32,
+}
+
+impl Reserved {
+    pub const fn new() -> Self {
+        Reserved { inner: 0 }
+    }
+}
+
+/// Like `&'_ T` but with move semantics and constrained access
+///
+/// We want move semantics to be able to kill the reference by `drop`-ing the
+/// value.
+pub struct Ref<T> {
+    inner: NonNull<T>,
+}
+
+impl<T> Ref<T> {
+    /// # Safety
+    /// Lifetime is lost. Lifetime must be tracked by the caller
+    pub unsafe fn new(p: &T) -> Self {
+        Self {
+            inner: NonNull::from(p),
+        }
+    }
+
+    /// # Safety
+    /// Lifetime must be tracked by the caller
+    pub unsafe fn new_unchecked(p: *const T) -> Self {
+        Self {
+            inner: NonNull::new_unchecked(p as *mut T),
+        }
+    }
+
+    pub fn as_ptr(&self) -> *const T {
+        self.inner.as_ptr()
+    }
+}
+
+impl<T> ops::Deref for Ref<T> {
+    type Target = T;
+
+    fn deref(&self) -> &T {
+        unsafe { self.inner.as_ref() }
+    }
+}
+
+/// Waits until `cond` returns true
+///
+/// Returns `Err` if the operation timed out
+pub fn wait(mut cond: impl FnMut() -> bool, timeout: Duration) -> Result<(), ()> {
+    let start = Instant::now();
+    while !cond() {
+        // instead of busy waiting flush the in-memory logger
+        crate::memlog_try_flush();
+
+        if start.elapsed() > timeout {
+            return Err(());
+        }
+    }
+    Ok(())
+}
+
+pub fn epaddr2dqhidx(ep_addr: EndpointAddress) -> usize {
+    2 * ep_addr.index() + if ep_addr.is_out() { 0 } else { 1 }
+}
+
+pub fn epaddr2endptmask(ep_addr: EndpointAddress) -> u32 {
+    if ep_addr.is_out() {
+        1 << ep_addr.index()
+    } else {
+        1 << (16 + ep_addr.index())
+    }
+}
+
+#[derive(Clone, Copy)]
+pub struct Data {
+    pub ep_in_complete: u16,
+    pub ep_setup: u16,
+    pub ep_out: u16,
+}
+
+impl fmt::Debug for Data {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let mut s = f.debug_struct("Data");
+        if self.ep_in_complete != 0 {
+            s.field("ep_in_complete", &Hex(self.ep_in_complete));
+        }
+        if self.ep_setup != 0 {
+            s.field("ep_setup", &Hex(self.ep_setup));
+        }
+        if self.ep_out != 0 {
+            s.field("ep_out", &Hex(self.ep_out));
+        }
+        s.finish()
+    }
+}
+
+impl From<Data> for PollResult {
+    fn from(data: Data) -> PollResult {
+        PollResult::Data {
+            ep_in_complete: data.ep_in_complete,
+            ep_out: data.ep_out,
+            ep_setup: data.ep_setup,
+        }
+    }
+}
+
+pub struct Hex<T>(pub T);
+
+impl fmt::Debug for Hex<u16> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        if self.0 <= 0xff {
+            write!(f, "{:#04x}", self.0)
+        } else {
+            write!(f, "{:#06x}", self.0)
+        }
+    }
+}
+
+impl fmt::Debug for Hex<u32> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        if self.0 <= 0xff {
+            write!(f, "{:#04x}", self.0)
+        } else if self.0 <= 0xffff {
+            write!(f, "{:#06x}", self.0)
+        } else {
+            write!(f, "{:#06x}_{:04x}", self.0 >> 16, self.0 & 0xffff)
+        }
+    }
+}
