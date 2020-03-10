6d67376b548cd48306f557f0f54c9f8f67fff98b
Jonas Schievink <jonas.schievink@ferrous-systems.com>
2020-03-10 19:03:37 +0100

    add more duct tape, assert fails for some reason

--- firmware/usbarmory/src/fs.rs
+++ firmware/usbarmory/src/fs.rs
@@ -1,14 +1,20 @@
 //! File system access.
 
-use crate::storage::{Block, ManagedBlockDevice};
+use crate::storage::{Block, ManagedBlockDevice, BLOCK_SIZE};
 use littlefs2::{
     consts,
     driver::Storage,
-    fs::{Filesystem, FilesystemAllocation},
+    fs::{self, FileAllocation, FileType, Filesystem, FilesystemAllocation, SeekFrom},
+    io::{self, Read, Write, Seek},
 };
 use memlog::memlog;
 
-const BLOCK_COUNT: usize = 30_580_736;
+/// Hardcoded filesystem block count.
+///
+/// This should be removed and calculated dynamically based on the partition size.
+///
+/// littlefs2 has a hard 2^32 Byte limit.
+const BLOCK_COUNT: usize = !0 / (BLOCK_SIZE as usize);
 
 /// Backing storage used by littlefs.
 pub struct LittleFsAlloc<D: ManagedBlockDevice> {
@@ -16,6 +22,7 @@ pub struct LittleFsAlloc<D: ManagedBlockDevice> {
 }
 
 impl<D: ManagedBlockDevice> LittleFsAlloc<D> {
+    /// Creates a new filesystem allocation.
     pub fn new() -> Self {
         Self {
             inner: Filesystem::allocate(),
@@ -31,10 +38,10 @@ pub struct LittleFs<'a, D: ManagedBlockDevice> {
 
 impl<'a, D: ManagedBlockDevice> LittleFs<'a, D> {
     /// Mounts a littlefs2 file system.
-    pub fn mount(alloc: &'a mut LittleFsAlloc<D>, blockdev: D) -> littlefs2::io::Result<Self> {
-        if blockdev.total_blocks() != BLOCK_COUNT as u64 {
+    pub fn mount(alloc: &'a mut LittleFsAlloc<D>, blockdev: D) -> io::Result<Self> {
+        if blockdev.total_blocks() < BLOCK_COUNT as u64 {
             memlog!(
-                "expected {} blocks, got {}",
+                "expected at least {} blocks, got {}",
                 BLOCK_COUNT,
                 blockdev.total_blocks()
             );
@@ -51,37 +58,178 @@ impl<'a, D: ManagedBlockDevice> LittleFs<'a, D> {
     /// Mounts a littlefs2 file system for the duration of a closure `f`.
     ///
     /// This API avoids the need for using `LittleFsAlloc`.
-    pub fn with<R>(
+    pub fn mount_and_then<R>(
         blockdev: D,
-        f: impl FnOnce(&mut LittleFs<'_, D>) -> R,
-    ) -> littlefs2::io::Result<R> {
+        f: impl FnOnce(&mut LittleFs<'_, D>) -> io::Result<R>,
+    ) -> io::Result<R> {
         let mut alloc = LittleFsAlloc::new();
         let mut fs = LittleFs::mount(&mut alloc, blockdev)?;
 
-        Ok(f(&mut fs))
+        f(&mut fs)
     }
 
     /// Formats `blockdev`, creating a fresh littlefs file system (this erases all data!).
-    pub fn format(blockdev: D) -> littlefs2::io::Result<()> {
+    pub fn format(blockdev: D) -> io::Result<()> {
         Filesystem::format(&mut LfsStorage { inner: blockdev })
     }
 
     /// Returns the available space in Bytes (approximated).
-    pub fn available_space(&mut self) -> littlefs2::io::Result<u64> {
-        self.fs.available_space(&mut self.storage).map(|space| space as u64)
+    pub fn available_space(&mut self) -> io::Result<u64> {
+        self.fs
+            .available_space(&mut self.storage)
+            .map(|space| space as u64)
+    }
+
+    /// Creates a new directory at `path`.
+    pub fn create_dir(&mut self, path: impl AsRef<[u8]>) -> io::Result<()> {
+        self.fs.create_dir(path.as_ref(), &mut self.storage)
+    }
+
+    /// Removes the file or directory at `path`.
+    pub fn remove(&mut self, path: impl AsRef<[u8]>) -> io::Result<()> {
+        self.fs.remove(path.as_ref(), &mut self.storage)
+    }
+
+    /// Returns an iterator over the contents of the directory at `path`.
+    pub fn read_dir<'r>(&'r mut self, path: impl AsRef<[u8]>) -> io::Result<ReadDir<'r, 'a, D>> {
+        self.fs
+            .read_dir(path.as_ref(), &mut self.storage)
+            .map(move |inner| ReadDir { fs: self, inner })
     }
 }
 
-pub struct File {}
+/// Allocation backing a `File` instance.
+pub struct FileAlloc<D: ManagedBlockDevice> {
+    inner: FileAllocation<LfsStorage<D>>,
+}
 
-impl File {
-    pub fn with<R, D: ManagedBlockDevice>(
-        fs: &mut LittleFs<'_, D>,
-        path: impl AsRef<[u8]>,
-        f: impl FnOnce(&mut File) -> R,
-    ) -> littlefs2::io::Result<R> {
-        todo!()
+impl<D: ManagedBlockDevice> FileAlloc<D> {
+    /// Creates a new file allocation.
+    pub fn new() -> Self {
+        Self {
+            inner: fs::File::allocate(),
+        }
     }
+}
+
+/// An open file.
+pub struct File<'a, 'fs, D: ManagedBlockDevice> {
+    inner: fs::File<'a, LfsStorage<D>>,
+    fs: &'a mut LittleFs<'fs, D>,
+}
+
+impl<'a, 'fs, D: ManagedBlockDevice> File<'a, 'fs, D> {
+    /// Opens the file at `path`.
+    pub fn open(
+        fs: &'a mut LittleFs<'fs, D>,
+        alloc: &'a mut FileAlloc<D>,
+        path: impl AsRef<[u8]>,
+    ) -> io::Result<Self> {
+        let mut inner = fs::File::open(path.as_ref(), &mut alloc.inner, &mut fs.fs, &mut fs.storage)?;
+        inner.seek(&mut fs.fs, &mut fs.storage, SeekFrom::Start(0))?;
+        Ok(Self {
+            inner,
+            fs,
+        })
+    }
+
+    /// Creates or overwrites a file at `path`.
+    pub fn create(
+        fs: &'a mut LittleFs<'fs, D>,
+        alloc: &'a mut FileAlloc<D>,
+        path: impl AsRef<[u8]>,
+    ) -> io::Result<Self> {
+        Ok(Self {
+            inner: fs::File::create(path.as_ref(), &mut alloc.inner, &mut fs.fs, &mut fs.storage)?,
+            fs,
+        })
+    }
+
+    /// Calls a closure with the file at `path`.
+    ///
+    /// This avoids having to use `FileAlloc`.
+    pub fn open_and_then<R>(
+        fs: &mut LittleFs<'a, D>,
+        path: impl AsRef<[u8]>,
+        f: impl FnOnce(&mut File<'_, '_, D>) -> io::Result<R>,
+    ) -> io::Result<R> {
+        let mut alloc = FileAlloc::new();
+        let mut file = File::open(fs, &mut alloc, path)?;
+
+        let res = f(&mut file);
+        file.close()?;
+        res
+    }
+
+    /// Calls a closure with a file created at `path`.
+    ///
+    /// This avoids having to use `FileAlloc`.
+    pub fn create_and_then<R>(
+        fs: &mut LittleFs<'a, D>,
+        path: impl AsRef<[u8]>,
+        f: impl FnOnce(&mut File<'_, '_, D>) -> io::Result<R>,
+    ) -> io::Result<R> {
+        let mut alloc = FileAlloc::new();
+        let mut file = File::create(fs, &mut alloc, path)?;
+
+        f(&mut file)
+    }
+
+    /// Consumes and closes the file.
+    pub fn close(self) -> io::Result<()> {
+        self.inner.close(&mut self.fs.fs, &mut self.fs.storage)
+    }
+
+    /// Returns the length of this file in Bytes.
+    pub fn len(&mut self) -> io::Result<usize> {
+        self.inner.len(&mut self.fs.fs, &mut self.fs.storage)
+    }
+
+    /// Reads bytes from this file into `buf`.
+    pub fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
+        self.inner.read(&mut self.fs.fs, &mut self.fs.storage, buf)
+    }
+
+    /// Writes byte from `buf` into this file.
+    pub fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+        self.inner.write(&mut self.fs.fs, &mut self.fs.storage, buf)
+    }
+
+    /// Flushes any data written to this file to the underlying storage.
+    pub fn flush(&mut self) -> io::Result<()> {
+        self.inner.flush(&mut self.fs.fs, &mut self.fs.storage)
+    }
+}
+
+/// An iterator over entries in a directory.
+pub struct ReadDir<'a, 'fs, D: ManagedBlockDevice> {
+    inner: fs::ReadDir<LfsStorage<D>>,
+    fs: &'a mut LittleFs<'fs, D>,
+}
+
+impl<'a, 'fs, D: ManagedBlockDevice> Iterator for ReadDir<'a, 'fs, D> {
+    type Item = littlefs2::io::Result<DirEntry<D>>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match self.inner.next(&mut self.fs.fs, &mut self.fs.storage) {
+            Some(res) => Some(res.map(|inner| DirEntry { inner })),
+            None => None,
+        }
+    }
+}
+
+/// A directory entry returned by `ReadDir`.
+pub struct DirEntry<D: ManagedBlockDevice> {
+    inner: fs::DirEntry<LfsStorage<D>>,
+}
+
+impl<D: ManagedBlockDevice> DirEntry<D> {
+    /// Returns the type of this entry.
+    pub fn file_type(&self) -> FileType {
+        self.inner.file_type()
+    }
+
+    // FIXME: Add filename access once the littlefs2 crate supports it.
 }
 
 struct LfsStorage<D: ManagedBlockDevice> {
@@ -95,12 +243,11 @@ impl<D: ManagedBlockDevice> Storage for LfsStorage<D> {
     type PATH_MAX_PLUS_ONE = consts::U256;
     type ATTRBYTES_MAX = consts::U1022;
 
-    const READ_SIZE: usize = 512;
-    const WRITE_SIZE: usize = 512;
-    const BLOCK_SIZE: usize = 512;
+    const READ_SIZE: usize = BLOCK_SIZE as usize;
+    const WRITE_SIZE: usize = BLOCK_SIZE as usize;
+    const BLOCK_SIZE: usize = BLOCK_SIZE as usize;
 
-    // FIXME: This really shouldn't be a constant. This value is only correct for the second
-    // partition on the eMMC, and only when following the instructions *exactly*.
+    // FIXME: This really shouldn't be a constant.
     const BLOCK_COUNT: usize = BLOCK_COUNT;
 
     // Disable wear leveling since the `ManagedBlockDevice` is assumed to already implement that.
@@ -151,40 +298,5 @@ impl<D: ManagedBlockDevice> Storage for LfsStorage<D> {
 
         // XXX this is ignored
         Ok(len)
-    }
-}
-
-/// Dummy storage used to avoid unnecessary type parameters.
-struct LfsDummyStorage {}
-
-impl Storage for LfsDummyStorage {
-    type CACHE_SIZE = consts::U512;
-    type LOOKAHEADWORDS_SIZE = consts::U16;
-    type FILENAME_MAX_PLUS_ONE = consts::U256;
-    type PATH_MAX_PLUS_ONE = consts::U256;
-    type ATTRBYTES_MAX = consts::U1022;
-
-    const READ_SIZE: usize = 512;
-    const WRITE_SIZE: usize = 512;
-    const BLOCK_SIZE: usize = 512;
-
-    // FIXME: This really shouldn't be a constant. This value is only correct for the second
-    // partition on the eMMC, and only when following the instructions *exactly*.
-    const BLOCK_COUNT: usize = BLOCK_COUNT;
-
-    // Disable wear leveling since the `ManagedBlockDevice` is assumed to already implement that.
-    const BLOCK_CYCLES: isize = -1;
-    const FILEBYTES_MAX: usize = 2_147_483_647;
-
-    fn read(&self, _off: usize, _buf: &mut [u8]) -> littlefs2::io::Result<usize> {
-        unimplemented!();
-    }
-
-    fn write(&mut self, _off: usize, _data: &[u8]) -> littlefs2::io::Result<usize> {
-        unimplemented!();
-    }
-
-    fn erase(&mut self, _off: usize, _len: usize) -> littlefs2::io::Result<usize> {
-        unimplemented!();
     }
 }
