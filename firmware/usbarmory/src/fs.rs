61cc0fab0c0b6f742a5265fb7b80fc4cad40ed13
Jorge Aparicio <jorge.aparicio@ferrous-systems.com>
2020-04-02 16:22:17 +0200

    make all fs and file ops work through shared references

--- firmware/usbarmory/src/fs.rs
+++ firmware/usbarmory/src/fs.rs
@@ -1,5 +1,7 @@
 //! File system access.
 
+use core::cell::RefCell;
+
 use crate::storage::{Block, ManagedBlockDevice, BLOCK_SIZE};
 use littlefs2::{
     consts,
@@ -15,7 +17,9 @@ use memlog::memlog;
 /// This should be removed and calculated dynamically based on the partition size.
 ///
 /// littlefs2 has a hard 2^32 Byte limit.
-const BLOCK_COUNT: usize = 131_072; // 64 MiB
+// NOTE if you modify this you may need to modify the size of the MBR partition; the MBR partition
+// must be bigger than this number
+const BLOCK_COUNT: usize = 131_072; // 64 MiB / 512 (=block_size)
 
 /// Backing storage used by littlefs.
 pub struct LittleFsAlloc<D: ManagedBlockDevice> {
@@ -39,8 +43,8 @@ impl<D: ManagedBlockDevice> Default for LittleFsAlloc<D> {
 
 /// A littlefs2 file system.
 pub struct LittleFs<'a, D: ManagedBlockDevice> {
-    storage: LfsStorage<D>,
-    fs: Filesystem<'a, LfsStorage<D>>,
+    storage: RefCell<LfsStorage<D>>,
+    fs: RefCell<Filesystem<'a, LfsStorage<D>>>,
 }
 
 impl<'a, D: ManagedBlockDevice> LittleFs<'a, D> {
@@ -56,8 +60,8 @@ impl<'a, D: ManagedBlockDevice> LittleFs<'a, D> {
             return Err(littlefs2::io::Error::NoSpace); // close enough?
         }
 
-        let mut storage = LfsStorage { inner: blockdev };
-        let fs = Filesystem::mount(&mut alloc.inner, &mut storage)?;
+        let mut storage = RefCell::new(LfsStorage { inner: blockdev });
+        let fs = RefCell::new(Filesystem::mount(&mut alloc.inner, storage.get_mut())?);
 
         Ok(Self { storage, fs })
     }
@@ -67,12 +71,12 @@ impl<'a, D: ManagedBlockDevice> LittleFs<'a, D> {
     /// This API avoids the need for using `LittleFsAlloc`.
     pub fn mount_and_then<R>(
         blockdev: D,
-        f: impl FnOnce(&mut LittleFs<'_, D>) -> io::Result<R>,
+        f: impl FnOnce(&LittleFs<'_, D>) -> io::Result<R>,
     ) -> io::Result<R> {
         let mut alloc = LittleFsAlloc::new();
-        let mut fs = LittleFs::mount(&mut alloc, blockdev)?;
+        let fs = LittleFs::mount(&mut alloc, blockdev)?;
 
-        f(&mut fs)
+        f(&fs)
     }
 
     /// Formats `blockdev`, creating a fresh littlefs file system (this erases all data!).
@@ -81,26 +85,32 @@ impl<'a, D: ManagedBlockDevice> LittleFs<'a, D> {
     }
 
     /// Returns the available space in Bytes (approximated).
-    pub fn available_space(&mut self) -> io::Result<u64> {
+    pub fn available_space(&self) -> io::Result<u64> {
         self.fs
-            .available_space(&mut self.storage)
+            .borrow_mut()
+            .available_space(&mut self.storage.borrow_mut())
             .map(|space| space as u64)
     }
 
     /// Creates a new directory at `path`.
-    pub fn create_dir(&mut self, path: impl AsRef<[u8]>) -> io::Result<()> {
-        self.fs.create_dir(path.as_ref(), &mut self.storage)
+    pub fn create_dir(&self, path: impl AsRef<[u8]>) -> io::Result<()> {
+        self.fs
+            .borrow_mut()
+            .create_dir(path.as_ref(), &mut self.storage.borrow_mut())
     }
 
     /// Removes the file or directory at `path`.
-    pub fn remove(&mut self, path: impl AsRef<[u8]>) -> io::Result<()> {
-        self.fs.remove(path.as_ref(), &mut self.storage)
+    pub fn remove(&self, path: impl AsRef<[u8]>) -> io::Result<()> {
+        self.fs
+            .borrow_mut()
+            .remove(path.as_ref(), &mut self.storage.borrow_mut())
     }
 
     /// Returns an iterator over the contents of the directory at `path`.
-    pub fn read_dir<'r>(&'r mut self, path: impl AsRef<[u8]>) -> io::Result<ReadDir<'r, 'a, D>> {
+    pub fn read_dir<'r>(&'r self, path: impl AsRef<[u8]>) -> io::Result<ReadDir<'r, 'a, D>> {
         self.fs
-            .read_dir(path.as_ref(), &mut self.storage)
+            .borrow_mut()
+            .read_dir(path.as_ref(), &mut self.storage.borrow_mut())
             .map(move |inner| ReadDir { fs: self, inner })
     }
 }
@@ -131,40 +141,48 @@ impl<D: ManagedBlockDevice> Default for FileAlloc<D> {
 /// while closing the file will result in a panic. Use the `close` method to handle IO errors
 /// instead of potentially panicking.
 pub struct File<'a, 'fs, D: ManagedBlockDevice> {
-    inner: Option<fs::File<'a, LfsStorage<D>>>,
-    fs: &'a mut LittleFs<'fs, D>,
+    inner: Option<RefCell<fs::File<'a, LfsStorage<D>>>>,
+    fs: &'a LittleFs<'fs, D>,
 }
 
 #[allow(clippy::len_without_is_empty)]
 impl<'a, 'fs, D: ManagedBlockDevice> File<'a, 'fs, D> {
     /// Opens the file at `path`.
     pub fn open(
-        fs: &'a mut LittleFs<'fs, D>,
+        fs: &'a LittleFs<'fs, D>,
         alloc: &'a mut FileAlloc<D>,
         path: impl AsRef<[u8]>,
     ) -> io::Result<Self> {
-        let mut inner =
-            fs::File::open(path.as_ref(), &mut alloc.inner, &mut fs.fs, &mut fs.storage)?;
-        inner.seek(&mut fs.fs, &mut fs.storage, SeekFrom::Start(0))?;
+        let mut inner = fs::File::open(
+            path.as_ref(),
+            &mut alloc.inner,
+            &mut fs.fs.borrow_mut(),
+            &mut fs.storage.borrow_mut(),
+        )?;
+        inner.seek(
+            &mut fs.fs.borrow_mut(),
+            &mut fs.storage.borrow_mut(),
+            SeekFrom::Start(0),
+        )?;
         Ok(Self {
-            inner: Some(inner),
+            inner: Some(RefCell::new(inner)),
             fs,
         })
     }
 
     /// Creates or overwrites a file at `path`.
     pub fn create(
-        fs: &'a mut LittleFs<'fs, D>,
+        fs: &'a LittleFs<'fs, D>,
         alloc: &'a mut FileAlloc<D>,
         path: impl AsRef<[u8]>,
     ) -> io::Result<Self> {
         Ok(Self {
-            inner: Some(fs::File::create(
+            inner: Some(RefCell::new(fs::File::create(
                 path.as_ref(),
                 &mut alloc.inner,
-                &mut fs.fs,
-                &mut fs.storage,
-            )?),
+                &mut fs.fs.borrow_mut(),
+                &mut fs.storage.borrow_mut(),
+            )?)),
             fs,
         })
     }
@@ -175,14 +193,14 @@ impl<'a, 'fs, D: ManagedBlockDevice> File<'a, 'fs, D> {
     ///
     /// NOTE the file will be `sync`-ed and `close`-d after `f` is executed
     pub fn open_and_then<R>(
-        fs: &mut LittleFs<'a, D>,
+        fs: &LittleFs<'a, D>,
         path: impl AsRef<[u8]>,
-        f: impl FnOnce(&mut File<'_, '_, D>) -> io::Result<R>,
+        f: impl FnOnce(&File<'_, '_, D>) -> io::Result<R>,
     ) -> io::Result<R> {
         let mut alloc = FileAlloc::new();
-        let mut file = File::open(fs, &mut alloc, path)?;
+        let file = File::open(fs, &mut alloc, path)?;
 
-        let res = f(&mut file);
+        let res = f(&file);
         file.close()?;
         res
     }
@@ -193,14 +211,14 @@ impl<'a, 'fs, D: ManagedBlockDevice> File<'a, 'fs, D> {
     ///
     /// NOTE the file will be `sync`-ed and `close`-d after `f` is executed
     pub fn create_and_then<R>(
-        fs: &mut LittleFs<'a, D>,
+        fs: &LittleFs<'a, D>,
         path: impl AsRef<[u8]>,
-        f: impl FnOnce(&mut File<'_, '_, D>) -> io::Result<R>,
+        f: impl FnOnce(&File<'_, '_, D>) -> io::Result<R>,
     ) -> io::Result<R> {
         let mut alloc = FileAlloc::new();
-        let mut file = File::create(fs, &mut alloc, path)?;
+        let file = File::create(fs, &mut alloc, path)?;
 
-        let r = f(&mut file)?;
+        let r = f(&file)?;
         file.close()?;
         Ok(r)
     }
@@ -216,41 +234,63 @@ impl<'a, 'fs, D: ManagedBlockDevice> File<'a, 'fs, D> {
         self.inner
             .take()
             .unwrap_or_else(|| unsafe { assume_unreachable!() })
-            .close(&mut self.fs.fs, &mut self.fs.storage)
+            .into_inner()
+            .close(
+                &mut self.fs.fs.borrow_mut(),
+                &mut self.fs.storage.borrow_mut(),
+            )
     }
 
     /// Returns the length of this file in Bytes.
-    pub fn len(&mut self) -> io::Result<usize> {
+    pub fn len(&self) -> io::Result<usize> {
         self.inner
-            .as_mut()
+            .as_ref()
             .unwrap_or_else(|| unsafe { assume_unreachable!() })
-            .len(&mut self.fs.fs, &mut self.fs.storage)
+            .borrow_mut()
+            .len(
+                &mut self.fs.fs.borrow_mut(),
+                &mut self.fs.storage.borrow_mut(),
+            )
     }
 
     /// Reads bytes from this file into `buf`.
-    pub fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
+    pub fn read(&self, buf: &mut [u8]) -> io::Result<usize> {
         self.inner
-            .as_mut()
+            .as_ref()
             .unwrap_or_else(|| unsafe { assume_unreachable!() })
-            .read(&mut self.fs.fs, &mut self.fs.storage, buf)
+            .borrow_mut()
+            .read(
+                &mut self.fs.fs.borrow_mut(),
+                &mut self.fs.storage.borrow_mut(),
+                buf,
+            )
     }
 
     /// Writes byte from `buf` into this file.
     ///
     /// NOTE writes are cached in memory; use `sync` to flush the cache to disk
-    pub fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+    pub fn write(&self, buf: &[u8]) -> io::Result<usize> {
         self.inner
-            .as_mut()
+            .as_ref()
             .unwrap_or_else(|| unsafe { assume_unreachable!() })
-            .write(&mut self.fs.fs, &mut self.fs.storage, buf)
+            .borrow_mut()
+            .write(
+                &mut self.fs.fs.borrow_mut(),
+                &mut self.fs.storage.borrow_mut(),
+                buf,
+            )
     }
 
     /// Synchronize file contents to storage
-    pub fn sync(&mut self) -> io::Result<()> {
+    pub fn sync(&self) -> io::Result<()> {
         self.inner
-            .as_mut()
+            .as_ref()
             .unwrap_or_else(|| unsafe { assume_unreachable!() })
-            .sync(&mut self.fs.fs, &mut self.fs.storage)
+            .borrow_mut()
+            .sync(
+                &mut self.fs.fs.borrow_mut(),
+                &mut self.fs.storage.borrow_mut(),
+            )
     }
 }
 
@@ -260,7 +300,13 @@ where
 {
     fn drop(&mut self) {
         if let Some(inner) = self.inner.take() {
-            inner.close(&mut self.fs.fs, &mut self.fs.storage).unwrap()
+            inner
+                .into_inner()
+                .close(
+                    &mut self.fs.fs.borrow_mut(),
+                    &mut self.fs.storage.borrow_mut(),
+                )
+                .unwrap()
         }
     }
 }
@@ -268,14 +314,17 @@ where
 /// An iterator over entries in a directory.
 pub struct ReadDir<'a, 'fs, D: ManagedBlockDevice> {
     inner: fs::ReadDir<LfsStorage<D>>,
-    fs: &'a mut LittleFs<'fs, D>,
+    fs: &'a LittleFs<'fs, D>,
 }
 
 impl<'a, 'fs, D: ManagedBlockDevice> Iterator for ReadDir<'a, 'fs, D> {
     type Item = littlefs2::io::Result<DirEntry<D>>;
 
     fn next(&mut self) -> Option<Self::Item> {
-        match self.inner.next(&mut self.fs.fs, &mut self.fs.storage) {
+        match self.inner.next(
+            &mut self.fs.fs.borrow_mut(),
+            &mut self.fs.storage.borrow_mut(),
+        ) {
             Some(res) => Some(res.map(|inner| DirEntry { inner })),
             None => None,
         }
