18c4abdc242ed82af565c4facf81bce16bcc803d
Tony Arcieri <tony@iqlusion.io>
2020-04-11 13:19:31 -0700

    macOS: use baud rate of 3,000,000 (supported by Apple driver)
    4 megabaud is not supported by whatever the Apple-supplied driver is
    used for FT4232H.
    
    Testing of standard baud rates shows it caps out at 3,000,000 baud:
    
        Running `usd-runner target/armv7a-none-eabi/debug/examples/hello`
        Hello, world!
        (device has reset)
    
    TODO: on Linux hosts, automatically configure 4Mbaud instead

--- host/usd/src/bin/usd-runner.rs
+++ host/usd/src/bin/usd-runner.rs
@@ -98,7 +98,7 @@ fn redirect() -> Result<(), anyhow::Error> {
     const VID: u16 = 0x0403; // Future Technology Devices International, Ltd
     const PID: u16 = 0x6011; // FT4232H Quad HS USB-UART/FIFO IC
     const DEVNO: usize = 2; // device #3 is the one we want
-    const BAUD_RATE: u32 = 4_000_000;
+    const BAUD_RATE: u32 = 3_000_000; // max baud rate supported on macOS (TODO: 4Mbaud on Linux hosts)
     const BUFSZ: usize = 512; // the FT4232H uses 512B USB packets
 
     let mut settings = SerialPortSettings::default();
